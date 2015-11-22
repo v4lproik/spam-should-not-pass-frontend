5b21e0d92782c875ba61577ae2e96d13b5d9f952
v4lproik <v4lproik@gmail.com>
2015-11-22 18:15:54 +0100

    Add default route to router paths

---
+++ app/components/Admin/rule_index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Link, IndexLink } from 'react-router';
+import LoginStore from '../../stores/LoginStore.js';
+import PlatformException from '../../models/PlatformException.js';
+import MemberInfoService from '../../services/MemberService.js';
+import SessionService from '../../services/SessionService.js';
+import RuleService from '../../services/RuleService.js';
+import {redirectionError, redirectionSessionExpired, redirectionUnauthorised} from '../App/utility.js';
+
+var Rule = React.createClass({
+
+    render: function() {
+        var style='display:block;float:right;color:yellow;';
+
+        return (
+            <div>
+                <section className="content-header">
+                    <h1>
+                        Rules
+                    </h1>
+                </section>
+
+                <section className="content">
+                    {this.props.children}
+                </section>
+            </div>
+        )
+    }
+});
+
+module.exports = Rule;
