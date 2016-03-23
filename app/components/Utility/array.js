45d5b0c6581146f64010d354e2a338d75c1d0c43
jrousseau <v4lproik@gmail.com>
2016-03-23 16:53:23 +0100

    [scheme] Add array.js

---
+++ app/components/Utility/array.js
@@ -0,0 +1,19 @@
+class ArrayUtility {
+
+    static sortByKey (array, key){
+        return array.sort(function(a, b) {
+            var x = a[key];
+            var y = b[key];
+
+            if (typeof x == "string")
+            {
+                x = x.toLowerCase();
+                y = y.toLowerCase();
+            }
+
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        });
+    }
+}
+
+export default ArrayUtility;
